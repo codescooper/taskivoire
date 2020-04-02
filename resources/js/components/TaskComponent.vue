a2eb44c4b70fc3a2c5930038c55b9a8198a5a14f
codescooper <codescooper@gmail.com>
2020-04-02 22:03:33 +0000

    creation des composants Home et Task et ajout d'une navbar

---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <ul class="list-group">
+                <li class="list-group-item"><a href="">tache 1</a></li>
+                <li class="list-group-item"><a href="">tache 2</a></li>
+                <li class="list-group-item"><a href="">tache 3</a></li>
+                <li class="list-group-item"><a href="">tache 4</a></li>
+                <li class="list-group-item"><a href="">tache 5</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
